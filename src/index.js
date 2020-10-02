e04d2f9bd4924b040f3f0b53b29bd86b4ec0086f
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-10-02 00:41:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const theForm = document.getElementById('create-task-form');
+  theForm.addEventListener('submit', (event) => {
+    let theTask = document.getElementById("new-task-description").value;
+    let theList = document.querySelector("div ul");
+    let theItem = document.createElement('li');
+    theItem.innerHTML = theTask;
+    theList.appendChild(theItem);
+    document.getElementById("new-task-description").value = "";
+    event.preventDefault();
+  });
+
 });
